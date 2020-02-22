770a3dc61a34f09205378628123cd6fe211f8719
sachin <sachinkshuklaoo7@gamil.com>
2020-02-22 23:53:44 +0530

    Started the project

---
+++ src/app.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SlimNoteApp from './components/SlimNoteApp';
+
+import './styles/styles.scss';
+
+ReactDOM.render(<SlimNoteApp />, document.getElementById('app'));
